806eb0c3792ec857477dda01a42d2c830e8f3f35
abdfha100 <abdul.shariff@hotmail.com>
2020-03-16 11:07:57 +0530

    first commit

---
+++ src/test/java/stepDefination/StepDefination.java
@@ -0,0 +1,39 @@
+package stepDefination;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.And;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class StepDefination {
+
+    @Given("^User is on NetBanking landing page$")
+    public void user_is_on_netbanking_landing_page() throws Throwable {
+        System.out.println("navigated to login URL");
+    }
+
+    @When("^User login into application with username \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void user_login_into_application_with_username_something_and_password_something(String strArg1, String strArg2) throws Throwable {
+    	System.out.println(strArg1);
+    	System.out.println(strArg2);
+        System.out.println("Logedd in success");
+    }
+
+    @Then("^Home page is populated$")
+    public void home_page_is_populated() throws Throwable {
+        //code to home page validation
+    	 System.out.println("validated the home page");
+    }
+    
+    @And("^Cards displayed are \"([^\"]*)\"$")
+    public void cards_displayed_are_something(String strArg1) throws Throwable {
+    	//code validating the cards..
+   	    System.out.println("validated the card");
+   	 System.out.println(strArg1);
+    }
+
+}
